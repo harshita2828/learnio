484b613bf6bf45763821853fc4232845b6d7317b
HARSHITA <93899633+harshita2828@users.noreply.github.com>
2024-12-12 20:27:41 +0530

    Update auth.js

--- server/Routes/auth.js
+++ server/Routes/auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const bcrypt = require("bcrypt");
-const User = require("../models/User");
+const User = require("../Models/User");
 
 dotenv.config();
 
